d81cbdfa71aa8082aff44393694216767102e775
anupshakya07 <anupshakya07@gmail.com>
2021-03-06 14:21:34 -0600

    Pushed Obj2Vec encoder and one hot encoder

---
+++ obj2vec_embedder.py
@@ -0,0 +1,57 @@
+import logging
+import pandas as pd
+import gensim.models
+import time
+
+
+class RelationWordEmbedding:
+    def __init__(self, ip_path, embedding_dimension=300, sliding_window_size=8):
+        self.input_file_path = ip_path
+        self.embedding_dimension = embedding_dimension
+        self.sliding_window_size = sliding_window_size
+        self.sentences = []
+        self.model = None
+
+    def generate_sentences(self):
+        chunk_iterator = pd.read_csv(self.input_file_path, sep="\t", header=0, iterator=True, chunksize=1000000)
+        print("------Generating Sentences to train the word embeddings-------")
+        start_time = time.time()
+
+        for chunk_data in chunk_iterator:
+            for student_id, std_groups in chunk_data.groupby('Anon Student Id'):
+                prob_hierarchy = std_groups.groupby('Problem Hierarchy')
+                for hierarchy, hierarchy_groups in prob_hierarchy:
+                    prob_name = hierarchy_groups.groupby('Problem Name')
+                    for problem_name, prob_name_groups in prob_name:
+                        sub_skills = prob_name_groups['KC(SubSkills)']
+                        for a in sub_skills:
+                            if str(a) != "nan":
+                                temp = a.split("~~")
+                                self.sentences.append(temp)  # {KC1,KC2,....,KCn}
+                                for kc in a.split("~~"):
+                                    list_with_single_kc = [student_id, hierarchy, problem_name, kc]
+                                    self.sentences.append(list_with_single_kc)  # {StdId, ProbHierarchy, ProblemName, KC}
+                                    st_kc = [student_id, kc]
+                                    self.sentences.append(st_kc)
+                                    prob_kc = [problem_name, kc]
+                                    self.sentences.append(prob_kc)
+        end_time = time.time()
+        print("Time Taken for generating the sentences = ", end_time - start_time)
+
+    def train(self):
+        self.generate_sentences()
+        print("******************************* Training Relational Word Embeddings *******************************")
+        print("Parameters:->>>>>>>")
+        print("Number of sentences trained = ", len(self.sentences))
+        print("Embedding Dimension of the Word Embeddings = ", self.embedding_dimension)
+        print("Sliding Window Size = ", self.sliding_window_size)
+        start_time = time.time()
+        self.model = gensim.models.Word2Vec(self.sentences, size=self.embedding_dimension, window=self.sliding_window_size, min_count = 1)
+        end_time = time.time()
+        print("Time taken to train the word embeddings = ", end_time - start_time, " secs.")
+
+    def save_trained_model(self, model_name):
+        self.model.save(model_name)
+
+    def load_trained_model(self, model_name):
+        self.model = gensim.models.Word2Vec.load(model_name)
---
+++ one_hot_encoder.py
@@ -0,0 +1,30 @@
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+
+
+class OneHotEnc:
+    def __init__(self):
+        self.one_hot_encoder = OneHotEncoder(sparse=False)
+        self.kc_dimension = None
+
+    def train(self, list_words):
+        np_array = np.array(list(list_words)).reshape(-1, 1)
+        self.one_hot_encoder.fit(np_array)
+        self.kc_dimension = len(list_words)
+
+    def transform_to_one_hot(self, kc_seq):
+        kc_2d = np.array(kc_seq).reshape(-1, 1)
+        one_hot = self.one_hot_encoder.transform(kc_2d)
+        return one_hot
+
+    def transform_one_hot_to_kcs(self, one_hot_list):
+        kcs_list = self.one_hot_encoder.inverse_transform(one_hot_list)
+        return kcs_list
+
+    def append_start_and_end_tokens(self, arr):
+        start_token = np.ones(self.kc_dimension)
+        end_token = np.ones(self.kc_dimension)
+        arr_list = arr.tolist()
+        arr_list.insert(0, start_token)
+        arr_list.append(end_token)
+        return np.array(arr_list)
