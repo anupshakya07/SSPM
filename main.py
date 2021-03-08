b8c946047c6f6e5bafad4a1ac5aa313fe431c21b
anupshakya07 <anupshakya07@gmail.com>
2021-03-08 13:06:49 -0600

    Testing OneHotEncoder

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from src import data_preprocessor
+from src import one_hot_encoder
 
 if __name__ == "__main__":
     train_file_path = "datasets/bridge_to_algebra_2008_2009/sample_bridge_to_algebra_2008_2009_train.txt"
     data_processor = data_preprocessor.DataPreprocessor(input_file_path=train_file_path)
     data_processor.analyze_dataset()
+
+    one_hot_encoder = one_hot_encoder.OneHotEnc()
+    one_hot_encoder.train(data_processor.unique_kcs)
+
+
--- src/one_hot_encoder.py
+++ src/one_hot_encoder.py
@@ -4,21 +4,21 @@ import numpy as np
 
 class OneHotEnc:
     def __init__(self):
-        self.one_hot_encoder = OneHotEncoder(sparse=False)
+        self.model = OneHotEncoder(sparse=False)
         self.kc_dimension = None
 
     def train(self, list_words):
         np_array = np.array(list(list_words)).reshape(-1, 1)
-        self.one_hot_encoder.fit(np_array)
+        self.model.fit(np_array)
         self.kc_dimension = len(list_words)
 
     def transform_to_one_hot(self, kc_seq):
         kc_2d = np.array(kc_seq).reshape(-1, 1)
-        one_hot = self.one_hot_encoder.transform(kc_2d)
+        one_hot = self.model.transform(kc_2d)
         return one_hot
 
     def transform_one_hot_to_kcs(self, one_hot_list):
-        kcs_list = self.one_hot_encoder.inverse_transform(one_hot_list)
+        kcs_list = self.model.inverse_transform(one_hot_list)
         return kcs_list
 
     def append_start_and_end_tokens(self, arr):
